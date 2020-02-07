ff16562cd2ede71e8578eb5489fc07dda8139283
Waqas Khushal <waqas.khushal@bp.com>
2020-02-07 12:22:41 +0000

    commit the blank project

---
+++ Interview/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interview
+{
+    // Please create an in memory implementation of IRepository<T, I> 
+
+    public interface IRepository<T, I> where T : IStoreable<I>
+    {
+        IEnumerable<T> GetAll();
+        T Get(I id);
+        void Delete(I id);
+        void Save(T item);
+    }
+}
---
+++ Interview/IStoreable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data;
+using System.Collections;
+
+namespace Interview
+{
+    public interface IStoreable<T>
+    {
+        T Id { get; set; }
+    }
+    
+}
